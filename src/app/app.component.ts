4a22cd4c928ca3123e166107158e2c2b754ea1d1
Hein Htike Aung <niziu1151998@gmail.com>
2022-06-29 13:38:31 +0630

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import * as JsBarcode from 'jsbarcode';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  barcode = '';
+
+  form: FormGroup;
+
   title = 'barcode';
+
+  constructor(private builder: FormBuilder) {
+    this.form = this.builder.group({
+      input: '',
+      type: 'code128'
+    })
+  }
+
+  generateBarCode() {
+    JsBarcode('#barcode', JSON.stringify(this.form.get('input').value), {
+      format: this.form.get('type').value,
+      displayValue: true,
+      lineColor: 'black',
+      width:1,
+      height: 40,
+      fontSize: 20,
+    });
+  }
+
+  printCode() {
+    var divContents = document.getElementById("barcodeprint").innerHTML;
+    var a = window.open('', '', '');
+    a.document.write('<html>');
+    a.document.write('<body > <h3>Bar code is </h3>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+  }
 }
